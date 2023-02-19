6d27d7a5c52edfedecae86b9dab5d443b0de1840
OysteinKoder <oystein.kodehode@gmail.com>
2023-02-19 14:16:53 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import logo from "./logo.svg";
-
-// Button is a default import, you can tell because it does not have curly brackets around it.
-
-import Button from "./components/Button";
 import "./App.css";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-// The paragraphs are named imports, you can tell because they have curly brackets
+// <BrowserRouter>:
+// This component provides a context for the application to use client-side routing using the HTML5 history API.
 
-import { ParagraphOne, ParagraphTwo } from "./components/Paragraphs";
+// <Link to="/about">:
+// This is a component provided by React Router that renders a clickable link to the "about" page of the application.
+// Clicking on this link will update the URL and trigger the rendering of the corresponding component.
 
-import { text } from "./language/text_example";
+//<Routes>:
+// This is a component provided by React Router that defines a set of routes for the application.
+// It renders the component corresponding to the current route based on the current URL.
+
+//<Route path="/about" element={<AboutPage />} />:
+// This is a component provided by React Router that defines a route for the "about" page of the application.
+// It specifies that when the URL matches the "/about" path, the AboutPage component should be rendered.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button />
-        <ParagraphOne text={text.TEXT_ONE} />
-        <ParagraphTwo text={text.TEXT_TWO} />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <Link to="/about">about</Link>
+            <Link to="/contact">contact</Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/language/text_example.js
+++
@@ -1,4 +0,0 @@
-export const text = {
-  TEXT_ONE: "this is text one",
-  TEXT_TWO: "this is text two",
-};
