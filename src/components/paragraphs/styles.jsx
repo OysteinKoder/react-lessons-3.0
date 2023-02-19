05cfb0474d0347cbd15a4f6588f0a5963f87755d
OysteinKoder <oystein.kodehode@gmail.com>
2023-02-19 11:21:53 +0100

    styled component

---
+++ src/components/paragraphs/styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+// StyledParagraph component.
+// Renders a styled paragraph element with specific CSS styles.
+// @returns {JSX.Element} - StyledParagraph component.
+
+export const StyledParagraph = styled.p`
+  font-size: 16px;
+  line-height: 1.5;
+  color: #61dafb;
+  margin-bottom: 1em;
+`;
