88fc604d54ef0685a129757f372c25bf7a3981a1
OysteinKoder <oystein.kodehode@gmail.com>
2023-03-27 13:55:18 +0200

    added documentation for counterPage

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -4,30 +4,38 @@ import { StyledButton } from "../components/button/styles";
 import { StyledParagraph } from "../components/paragraphs/styles";
 
 const CounterPage = () => {
+  // Hook that creates a state and a function to update it
   const [count, setCount] = useState(0);
 
+  // Function that increments the count
   const increment = () => {
     setCount((count) => count + 1);
-    console.log("increment");
   };
 
+  // Function that decrements the count
   const decrement = () => {
     setCount((count) => count - 1);
-    console.log("decrement");
   };
 
   return (
     <>
+      {/* Paragraph element that displays the current count */}
       <StyledParagraph
+        // If the count is positive, the text color is green
+        // If the count is negative, the text color is red
         style={
           count > 0 ? { color: "green" } : count < 0 ? { color: "red" } : null
         }
       >
         {count}
       </StyledParagraph>
+
+      {/* Button that calls the increment function */}
       <StyledButton onClick={increment}>
         {TEXT.COUNTER_PAGE.INCREMENT_BUTTON}
       </StyledButton>
+
+      {/* Button that calls the decrement function */}
       <StyledButton onClick={decrement}>
         {TEXT.COUNTER_PAGE.DECREMENT_BUTTON}
       </StyledButton>
